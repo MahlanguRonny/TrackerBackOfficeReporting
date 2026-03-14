f4458e9941501eabb728995e75828369d3b3ee65
Ronny.Mahlangu@zeda.co.za <Ronny.Mahlangu@zeda.co.za>
2026-03-14 11:42:44 +0200

    injected context and logger into the trader service

--- TrackerBackOfficTrader/Trader.Backend.Api/Services/TraderService.cs
+++ TrackerBackOfficTrader/Trader.Backend.Api/Services/TraderService.cs
@@ -1,10 +1,19 @@
-﻿using Trader.Backend.Api.Contracts.Requests;
+﻿using Trader.Backend.Api.AppContext;
+using Trader.Backend.Api.Contracts.Requests;
 using Trader.Backend.Api.Contracts.Responses;
 
 namespace Trader.Backend.Api.Services
 {
     public class TraderService : ITrader
     {
+        private readonly TraderAppContext _context;
+        private readonly ILogger<TraderService> _logger;
+
+        public TraderService(TraderAppContext context, ILogger<TraderService> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
         public Task<TradeTransationResponse> AddTradeTransaction(CreateTradeTransactionRequest createTradeTransaction)
         {
             throw new NotImplementedException();
